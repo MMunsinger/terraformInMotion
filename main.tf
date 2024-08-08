63180e573d6cc8d905538fef0846a2af47e737ca
MMunsinger <matthew.munsinger@gmail.com>
2024-08-08 13:27:35 -0500

    alksdjflasjdflkjfa

--- main.tf
+++ main.tf
@@ -53,7 +53,7 @@ resource "azurerm_network_interface" "example" {
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_virtual_network.example.subnet.subnet11.subnet_id
+    subnet_id                     = azurerm_virtual_network.example.subnet_id[0]
     private_ip_address_allocation = "Dynamic"
   }
 }
